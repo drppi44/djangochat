68f39c3c4b46425911a850f5d8c7c1f5f4c1cd6e
drppi44 <multinigel@gmail.com>
2015-10-05 19:14:36 +0300

    home page

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'apps.main',
 )
 
@@ -81,5 +82,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'assets'),
+)
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
--- djangochat/urls.py
+++ djangochat/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'djangochat.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
+
+    url(r'^$', 'apps.main.views.home_view', name='home'),
+
 
     url(r'^admin/', include(admin.site.urls)),
 )
