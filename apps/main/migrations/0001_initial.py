3edad1c40613559ffdfbbd3215b965e4298ae169
drppi44 <multinigel@gmail.com>
2015-10-10 03:07:00 +0300

    reg

---
+++ apps/main/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Message',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('datetime', models.DateTimeField(auto_created=True)),
+                ('name', models.CharField(max_length=55)),
+                ('text', models.TextField()),
+            ],
+        ),
+    ]
